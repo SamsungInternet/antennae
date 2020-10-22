22d8851a2baafaddb7d30edd99b5bc2b09c2f9ef
Ada Rose Cannon <ada@ada.is>
2020-10-22 14:01:45 +0100

    add a json file for search

--- .eleventy.js
+++ .eleventy.js
@@ -1,29 +1,54 @@
+const striptags = require("striptags");
+
+function extractExcerpt(article) {
+	if (!article.hasOwnProperty("templateContent")) {
+		console.warn(
+			'Failed to extract excerpt: Document has no property "templateContent".'
+		);
+		return null;
+	}
+
+	let excerpt = null;
+	const content = article.templateContent;
+
+	excerpt = striptags(content)
+		.replace(/^\\s+|\\s+$|\\s+(?=\\s)/g, "")
+		.replace(/\n/g, " ")
+		.trim();
+	
+	console.log(excerpt);
+	
+	return excerpt;
+}
+
+function appIcon(value) {
+	const data = value.data || Array.isArray(value) ? value[1] : value;
+	return new URL(
+		data.icons.sort(function (a, b) {
+			const aN = Number(a.sizes.split("x")[0]);
+			const bN = Number(b.sizes.split("x")[0]);
+			return bN - aN;
+		})[0].src,
+		data.baseurl
+	).href;
+	return value;
+}
+
 module.exports = function (eleventyConfig) {
-
 	eleventyConfig.addPassthroughCopy("images");
-    eleventyConfig.addPassthroughCopy("styles");
-	eleventyConfig.setTemplateFormats("html,11ty.js,md,njk");
+	eleventyConfig.addPassthroughCopy("styles");
+	eleventyConfig.setTemplateFormats("html,11ty.js,md,njk,json");
 
-	eleventyConfig.addFilter("appicon", function (value) {
-        const data = value.data || Array.isArray(value) ? value[1] : value;
-		return new URL(
-			data.icons.sort(function (a, b) {
-				const aN = Number(a.sizes.split("x")[0]);
-				const bN = Number(b.sizes.split("x")[0]);
-				return bN - aN;
-			})[0].src,
-			data.baseurl
-		).href;
-		return value;
-    });
-    
+	eleventyConfig.addFilter("appicon", appIcon);
+	eleventyConfig.addFilter("excerpt", extractExcerpt);
+
 	return {
-		pathPrefix: '/antennae/',
+		pathPrefix: "/antennae/",
 		passthroughFileCopy: true,
-        dir: {
+		dir: {
 			input: "src",
 			output: "docs",
-			includes: "_includes"
-        }
-    }
+			includes: "_includes",
+		},
+	};
 };
